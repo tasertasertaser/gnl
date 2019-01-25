307338db09bbe60a029476e1557dc6428de6c8f0
Crystal Schuller <cschulle@e1z3r2p2.42.us.org>
2019-01-24 17:15:12 -0800

    OH BOY HERE WE GO

--- get_next_line.c
+++ get_next_line.c
@@ -5,82 +5,86 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cschulle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/31 12:13:59 by cschulle          #+#    #+#             */
-/*   Updated: 2019/01/18 21:08:25 by cschulle         ###   ########.fr       */
+/*   Created: 2019/01/24 13:50:02 by cschulle          #+#    #+#             */
+/*   Updated: 2019/01/24 16:55:46 by cschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 /*
- * WHAT THIS DOES
-**
+** WHAT THIS DOES
+** -
 ** 1. takes pointer-to-pointer (**line) and file descriptor (fd) as argument
 ** 2. reads BUFF_SIZE characters into a buffer
-** 3. adds contents of buffer to string at **line pointer and continues reading until a '\n' is found
-** 		• given **line now points at a single line of the given file
-** 4. stores any remaining buf-read characters in a static array for next function call
-** 5. return value (int) is just a flag for whether operation was completed successfully
+** 3. adds contents of buf to string (**line), continuing until '\n' is found
+** 		• **line now points at 1 line of the file (to be printed by main())
+** 4. stores remaining buf chars in a static array (extra[fd]) for next fn call
+** 5. return value (int) is just a flag for whether op was successful
 **		•  1 = line was read
 **		•  0 = file has been completely read
 **		• -1 = an error occurred
-**
-**/
-#include <stdio.h> // don't forget to remove
+** -
+*/
 #include "get_next_line.h"
 
-int get_next_line(const int fd, char **line)
+void	ft_strcjoinfree(char **old, char *new, char c)
+{
+	char	*tmp;
+
+	tmp = *old;
+	*old = ft_strcjoin(*old, new, c);
+	free(tmp);
+}
+
+int		bufreader(const int fd, char **extra, char **line)
 {
 	char			buf[BUFF_SIZE + 1];
-	static char		*extra[FD_MAX];
 	size_t			i;
 
-	buf[BUFF_SIZE] = '\0';
-	if (!line || fd < 0 || read(fd, buf, 0) < 0) //error check
-		return(-1);
-	*line = NULL;
-	if (extra[fd])
-	{
-		*line = ft_strdup(extra[fd]);
-		extra[fd] = "\0";
-	}
 	while ((i = read(fd, buf, BUFF_SIZE)))
 	{
-		//printf("buf: %s\n", buf);
-		if (!(ft_strnchr(buf, '\n', BUFF_SIZE))) // if there is NOT a '\n' in buf
-		{
+		buf[i] = '\0';
+		if (!(ft_strchr(buf, '\n')))
 			if (!*line)
 				*line = ft_strdup(buf);
 			else
-				*line = ft_strjoin(*line, buf); // put buf into *line
-		//	printf(GRY"*line: %s\n", *line);
-		}
+				ft_strcjoinfree(line, buf, '\0');
 		else
 		{
-			if(!*line)
-				*line = "\0";
-			*line = ft_strcjoin(*line, buf, '\n');
-			extra[fd] = ft_strdup((ft_strchr(buf, '\n')) + 1);	
-			return(1);
+			(!*line) ? *line = ft_strdup("\0") : 0;
+			ft_strcjoinfree(line, buf, '\n');
+			(extra[fd]) ? free(extra[fd]) : 0;
+			extra[fd] = ft_strdup((ft_strchr(buf, '\n')) + 1);
+			return (1);
 		}
-		ft_bzero(buf, BUFF_SIZE);
 	}
-//	printf(GRY"line: %s\n", *line);
-	if (*line == NULL || **line == '\0')
+	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char		*extra[FD_MAX + 1];
+
+	if (!line || fd < 0 ||
+			read(fd, extra[fd], 0) < 0 || !(*line = ft_strnew(1)))
+		return (-1);
+	if (extra[fd])
+	{
+		if ((extra[FD_MAX] = ft_strchr(extra[fd], '\n')))
+		{
+			extra[FD_MAX][0] = '\0';
+			*line = ft_strdup(extra[fd]);
+			extra[fd] = ft_strdup(extra[FD_MAX] + 1);
+			return (1);
+		}
+		(*line) ? free(*line) : 0;
+		*line = ft_strdup(extra[fd]);
+		(extra[fd]) ? free(extra[fd]) : 0;
+		extra[fd] = ft_strdup("\0");
+	}
+	if (bufreader(fd, extra, line) == 1)
+		return (1);
+	else if (*line == NULL || **line == '\0')
 		return (0);
 	else
-		return(1);
-//	extra[fd] = "\0";
-//	return (0);
+		return (1);
 }
-/*
-int	main(void) // Don't forget to remove main()
-{
-	char **line;
- 	char *file = "waffles.txt";
-	int fd = 0; //open(file, O_RDONLY);
-	int i = 1;
-
-	while((i = get_next_line(fd, line)))
-		printf(WHT"%s "RED"%d\n"WHT, *line, i);
-	//printf("%s\n", *line);
-	printf(RED"%d\n"WHT, i);
-	return(0);
-}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,18 @@
 /*   By: cschulle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/31 12:31:31 by cschulle          #+#    #+#             */
-/*   Updated: 2019/01/13 18:49:51 by cschulle         ###   ########.fr       */
+/*   Updated: 2019/01/24 17:12:58 by cschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "libft/libft.h"
-#include <fcntl.h>
+# include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
 
-#define BUFF_SIZE 3
-#define FD_MAX 4864
-#define MCHECK(x) if (!x) return (-1);
-
-#define RED "\e[0;31m"
-#define GRY "\e[38;5;240m"
-#define YLW "\e[38;5;220m"
-#define WHT "\e[0m"
+# define BUFF_SIZE 8
+# define FD_MAX 4864
 
 #endif
